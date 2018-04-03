5c71960140cfc2eef933dad803bca6c99237707a
Eva <eva.bourgeais--boon@etu.univ-nantes.fr>
2018-04-03 22:33:29 +0200

    Finalized the analysis tool

--- src/controller/StatsController.java
+++ src/controller/StatsController.java
@@ -6,11 +6,14 @@ import java.util.ResourceBundle;
 import application.Main;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Alert.AlertType;
+import javafx.geometry.HPos;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Region;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 import model.Type;
@@ -19,8 +22,8 @@ import utils.IStats;
 
 public class StatsController implements Initializable{
 
-	private @FXML GridPane grid;
 	private @FXML Button buttonCloseStats;
+	private @FXML Pane centralPane;
 
 	private Main main;
 	private Stage window;
@@ -34,17 +37,52 @@ public class StatsController implements Initializable{
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
-		IStats.getInstance().setParameters(main);
-		IStats.update();
-		//System.out.println(IStats.getCategories());
-		//System.out.println(IStats.getInterviews());
+		
+		IStats.getInstance().update(main);
+		GridPane statsGrid = new GridPane();
+		statsGrid.setAlignment(Pos.CENTER);
+		statsGrid.setHgap(25);
+		statsGrid.setVgap(15);
+		
+		
+		/* init columns */
+		for (int i = 0; i < IStats.getInstance().getCategories().size(); i++) {
+			Label l_cat = new Label(IStats.getCategories().get(i).getName());
+			l_cat.setMinWidth(Region.USE_PREF_SIZE);
+			l_cat.setMaxWidth(Region.USE_PREF_SIZE);
+			statsGrid.setHalignment(l_cat, HPos.CENTER);
+			statsGrid.add(l_cat, i+1, 0);
+		}
+		
+		/* init rows */
+		for (int j = 0; j < IStats.getInstance().getInterviews().size(); j++) {
+			Label l_int = new Label(IStats.getInterviews().get(j).getName());
+			l_int.setMinWidth(Region.USE_PREF_SIZE);
+			l_int.setMaxWidth(Region.USE_PREF_SIZE);
+			statsGrid.setHalignment(l_int, HPos.CENTER);
+			statsGrid.add(l_int, 0, j+1);
+		}
+		
+		/* set values of for each columns and rows */
+		for (int i = 0; i < IStats.getInstance().getInterviews().size(); i++) {
+			for (int j = 0; j < IStats.getInstance().getCategories().size(); j++) {
+				int val = IStats.getInstance().nbOccurrences(IStats.getInstance().getInterviews().get(i), IStats.getInstance().getCategories().get(j));
+				Label l_val = new Label(String.valueOf(val));
+				statsGrid.setHalignment(l_val, HPos.CENTER);
+				statsGrid.add(l_val, j+1, i+1);
+			}
+				
+		}
+		
+		/* display all */
+		this.centralPane.getChildren().add(statsGrid);
 		buttonCloseStats.setText(main._langBundle.getString("close"));
 	}
 	
 	@FXML
     private void closeStats() {
-       window.close();
+		window.close();
     };
-
+    
 }
 
--- src/utils/IStats.java
+++ src/utils/IStats.java
@@ -2,7 +2,12 @@ package utils;
 
 import application.Main;
 import java.util.List;
+import java.util.Map;
+
+import com.sun.org.glassfish.gmbal.Description;
+
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
 import model.Category;
@@ -17,64 +22,120 @@ public class IStats {
     private IStats() {}
      
     private static IStats instance = null;
-    private Main main = null;
     private ArrayList<Category> mCategories = null;
     private ArrayList<DescriptionInterview> mInterviews = null;
+    private ArrayList<MomentExperience> mMomentsTmp = null;
+    private Map<DescriptionInterview, ArrayList<MomentExperience>> mItwMoments = null;
+    
+    
     
     public static IStats getInstance() {           
         if (instance == null)
         {   instance = new IStats();
         	instance.mCategories = new ArrayList<Category>();
         	instance.mInterviews = new ArrayList<DescriptionInterview>();
+        	instance.mItwMoments = new HashMap<DescriptionInterview, ArrayList<MomentExperience>>();
+        	instance.mMomentsTmp = new ArrayList<MomentExperience>();
         }
         return instance;
     }
     
-    public static void setParameters(Main main) {
-    	instance.main = main;
-    }
+   
+    /***** REFRESHING METHODS *****/
     
     /**
-     * Counts the number of occurences of appar
-     * @param interview the current interview
-     * @param category the current category of the schema 
-     * @return the number of times the category has been used in one specific interview
+     * finds the categories of the current scheme and stores it locally
+     * @param t the current type of the scheme
      */
-    public static int nbOccurences(DescriptionInterview interview, Category category) {
-    	int cpt = 0;
-    	for (MomentExperience moment : interview.getMoments()) {
-    			if (moment.getTypes().contains(category)) {
-    			cpt++;
-    		}
-		}
-    	return cpt;
-    }
-    
     private static void lookingForCategories(Type t) {
     	
-    	for (Type t_it : t.getTypes()) {
+    	for (Type t_it : t.getTypes()) { //for each type of the same level of the scheme
     					
 		   	if(t_it.isCategory()) {
-		   		instance.mCategories.add((Category) t_it);
+		   		instance.mCategories.add((Category) t_it); // if it is a category, we can store it (that is what we are looking for)
 		   	}
     	
 	    	else {
-	    		instance.lookingForCategories(t_it);
+	    		instance.lookingForCategories(t_it); // because one category can not be parent of an other one, we stop here and we do it again for the other types
 	    	}
     	}	   	
     }
     
+    
+    /**
+     * finds the interviews of the current project and stores it locally
+     * @param interviews the list of all the interviews
+     */
     private static void lookingForInterviews(LinkedList <DescriptionInterview> interviews) {
     	for (DescriptionInterview interview_it : interviews) {
     		instance.mInterviews.add(interview_it);
+    		instance.mItwMoments.put(interview_it, null);
 		}
     }
     
     
-    public static void update() {
-    	instance.lookingForCategories(instance.main.getCurrentProject().getSchema());
-    	instance.lookingForInterviews(instance.main.getCurrentProject().getInterviews());
+    /**
+     * finds all the moments and submoments of one specific interview and stores it locally in a temporary variable
+     * @param moment the current moment.
+     */
+    private static void lookingForMoments(MomentExperience moment) {
+    	instance.mMomentsTmp.add(moment);
+    	for (MomentExperience m_it : moment.getSubMoments()) {
+			instance.lookingForMoments(m_it);
+		}
     }
+    
+  
+    
+    /***** STATISTICS COMPUTING *****/ 
+    
+    
+    /**
+     * refresh the singleton's attributes
+     * @param main
+     */
+    public static void update(Main main) {
+    	instance.mCategories.clear();
+    	instance.mInterviews.clear();
+    	instance.mItwMoments.clear();
+    	instance.lookingForCategories(main.getCurrentProject().getSchema());
+    	instance.lookingForInterviews(main.getCurrentProject().getInterviews());
+    }
+    
+    /**
+     * Counts the number of occurences of appearance of a specific category in a specific interview
+     * @param interview the current interview
+     * @param category the current category of the schema 
+     * @return the number of times the category has been used in one specific interview
+     */
+    public static int nbOccurrences(DescriptionInterview interview, Category category) {
+    	
+    	int cpt = 0;
+    	    	
+    	/* if we are looking for a category in an interview that we have not met yet, we have to find all the moment*/
+    	
+    	if (instance.mItwMoments.get(interview) == null) {
+    		
+    		for (MomentExperience mom_it : interview.getMoments()) { // we start with the first-level moments
+    			instance.lookingForMoments(mom_it); //then we find all the sub-moments employed in the model of the concerned interview
+    		}
+    		
+    		instance.mItwMoments.put(interview, new ArrayList<MomentExperience>(instance.mMomentsTmp)); //we copy only the values of the moments found for 1 interview
+    		instance.mMomentsTmp.clear(); //we clear it because we need it free for the next interview
+    	
+    	}
+    	
+    	for (MomentExperience moment : instance.mItwMoments.get(interview)) {
+	    	if (moment.getTypes().contains(category)) { //then we compare if we find any category of the scheme in the model
+				cpt++;
+			}
+    	}
+    	
+    	return cpt;
+    }
+    
+
+    /***** GETTERS *****/ 
     
     public static ArrayList<Category> getCategories() {
     	return instance.mCategories;
