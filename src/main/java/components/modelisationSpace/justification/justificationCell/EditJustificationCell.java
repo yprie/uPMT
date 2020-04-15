9200f573a36ce3642570f8fa8002401e0f8b5706
Xavier <xavier.tremillon.pro@gmail.com>
2020-04-15 18:03:31 +0200

    added simple edit menu for descripteme

--- src/main/java/components/modelisationSpace/justification/justificationCell/EditJustificationCell.java
+++ src/main/java/components/modelisationSpace/justification/justificationCell/EditJustificationCell.java
@@ -1,9 +1,16 @@
 package components.modelisationSpace.justification.justificationCell;
 
 import application.configuration.Configuration;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
@@ -18,14 +25,38 @@ import java.util.ResourceBundle;
 public class EditJustificationCell implements Initializable {
 
     private Descripteme descripteme;
+    private Descripteme descriptemeCopy;
+
+    private @FXML Label descriptemeText;
+    private @FXML Button shiftLeft, shiftRight;
+    private @FXML RadioButton beginningButton, endButton;
 
     public EditJustificationCell(Descripteme descripteme) {
         this.descripteme = descripteme;
+        this.descriptemeCopy = descripteme.duplicate();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        descriptemeText.textProperty().bind(descriptemeCopy.getSelectionProperty());
 
+        shiftLeft.setOnAction(actionEvent -> {
+            if(beginningButton.isSelected()){
+                descriptemeCopy.modifyIndex(descriptemeCopy.getStartIndex()-1, descriptemeCopy.getEndIndex());
+            }
+            else if(endButton.isSelected()){
+                descriptemeCopy.modifyIndex(descriptemeCopy.getStartIndex(), descriptemeCopy.getEndIndex()-1);
+            }
+        });
+
+        shiftRight.setOnAction(actionEvent -> {
+            if(beginningButton.isSelected()){
+                descriptemeCopy.modifyIndex(descriptemeCopy.getStartIndex()+1, descriptemeCopy.getEndIndex());
+            }
+            else if(endButton.isSelected()){
+                descriptemeCopy.modifyIndex(descriptemeCopy.getStartIndex(), descriptemeCopy.getEndIndex()+1);
+            }
+        });
     }
 
     public static EditJustificationCell edit(Descripteme descripteme, Window primaryStage) {
@@ -49,6 +80,10 @@ public class EditJustificationCell implements Initializable {
             e.printStackTrace();
         }
         return null;
+    }
+
+    private void onRadioButtonChange() {
+        //TODO disable a button if it is to a border of the entire text.
     }
 
 }
