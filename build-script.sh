cc6006abd0d9e61370c3faf63fa69f915f97dde7
Nathan <nathan.seva@outlook.fr>
2020-04-25 08:03:57 +0200

    Remove useless files

--- build-script.sh
+++
@@ -1,4 +0,0 @@
-./gradlew -b build-travis.gradle build --warning-mode all --scan
-./gradlew -b build-travis.gradle jar --warning-mode all --scan
-./gradlew -b build-travis.gradle jre --warning-mode all --scan
-./gradlew -b build-travis.gradle runtimeZip --warning-mode all --scan
