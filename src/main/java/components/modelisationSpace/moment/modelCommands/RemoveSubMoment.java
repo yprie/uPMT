dee34421703c095dbf7fe77107420cf43545d52c
Thykof <thykof@protonmail.ch>
2020-09-30 17:44:26 +0200

    Un-underline descripteme

--- src/main/java/components/modelisationSpace/moment/modelCommands/RemoveSubMoment.java
+++ src/main/java/components/modelisationSpace/moment/modelCommands/RemoveSubMoment.java
@@ -20,12 +20,34 @@ public class RemoveSubMoment extends ModelUserActionCommand {
     public Void execute() {
         oldIndex = parent.indexOf(moment);
         parent.removeMoment(moment);
+
+        // Remove the underlining of the descripteme deleted
         moment.getJustification().descriptemesProperty().forEach(descripteme -> {
             descripteme.getInterviewText().removeDescripteme(descripteme);
+        });
+        moment.concreteCategoriesProperty().forEach(concreteCategory -> {
+            concreteCategory.getJustification().descriptemesProperty().forEach(descripteme -> {
+                descripteme.getInterviewText().removeDescripteme(descripteme);
+            });
+            concreteCategory.propertiesProperty().forEach(concreteProperty -> {
+                concreteProperty.getJustification().descriptemesProperty().forEach(descripteme -> {
+                    descripteme.getInterviewText().removeDescripteme(descripteme);
+                });
+            });
         });
         moment.momentsProperty().forEach(subMoment -> {
             subMoment.getJustification().descriptemesProperty().forEach(descripteme -> {
                 descripteme.getInterviewText().removeDescripteme(descripteme);
+            });
+            subMoment.concreteCategoriesProperty().forEach(concreteCategory -> {
+                concreteCategory.getJustification().descriptemesProperty().forEach(descripteme -> {
+                    descripteme.getInterviewText().removeDescripteme(descripteme);
+                });
+                concreteCategory.propertiesProperty().forEach(concreteProperty -> {
+                    concreteProperty.getJustification().descriptemesProperty().forEach(descripteme -> {
+                        descripteme.getInterviewText().removeDescripteme(descripteme);
+                    });
+                });
             });
         });
         return null;
@@ -34,12 +56,34 @@ public class RemoveSubMoment extends ModelUserActionCommand {
     @Override
     public Void undo() {
         parent.addMoment(oldIndex, moment);
+
+        // Add the underlining of the descripteme deleted
         moment.getJustification().descriptemesProperty().forEach(descripteme -> {
             descripteme.getInterviewText().addDescripteme(descripteme);
+        });
+        moment.concreteCategoriesProperty().forEach(concreteCategory -> {
+            concreteCategory.getJustification().descriptemesProperty().forEach(descripteme -> {
+                descripteme.getInterviewText().addDescripteme(descripteme);
+            });
+            concreteCategory.propertiesProperty().forEach(concreteProperty -> {
+                concreteProperty.getJustification().descriptemesProperty().forEach(descripteme -> {
+                    descripteme.getInterviewText().addDescripteme(descripteme);
+                });
+            });
         });
         moment.momentsProperty().forEach(subMoment -> {
             subMoment.getJustification().descriptemesProperty().forEach(descripteme -> {
                 descripteme.getInterviewText().addDescripteme(descripteme);
+            });
+            subMoment.concreteCategoriesProperty().forEach(concreteCategory -> {
+                concreteCategory.getJustification().descriptemesProperty().forEach(descripteme -> {
+                    descripteme.getInterviewText().addDescripteme(descripteme);
+                });
+                concreteCategory.propertiesProperty().forEach(concreteProperty -> {
+                    concreteProperty.getJustification().descriptemesProperty().forEach(descripteme -> {
+                        descripteme.getInterviewText().addDescripteme(descripteme);
+                    });
+                });
             });
         });
         return null;
