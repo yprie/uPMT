a0f98f72cb6ede27a75bf6c648995bc588697dd3
E176504C <yves.motteux@etu.univ-nantes.fr>
2021-03-19 12:06:06 +0100

    fixed syntax error

--- src/main/java/components/rootLayout/Controllers/RootLayoutController.java
+++ src/main/java/components/rootLayout/Controllers/RootLayoutController.java
@@ -315,30 +315,30 @@ public class RootLayoutController implements Initializable {
 				case "_US_UserDefined_251":
 					Locale.setDefault(Locale.FRENCH);
 					System.out.println("switched to fr");
-					keyCombUNDO = new KeyCodeCombination(KeyCode.W, KeyCodeCombination.CONTROL_DOWN);
+					keyCombUNDO = new KeyCodeCombination(KeyCode.W, KeyCodeCombination.SHORTCUT_DOWN);
 					break;
 				case "de_AT":
 				case "de":
 				case "de_CH":
 					Locale.setDefault(Locale.GERMAN);
 					System.out.println("switched to de");
-					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.SHORTCUT_DOWN);
 					break;
 				case "it__Pro":
 				case "it":
 					Locale.setDefault(Locale.ITALIAN);
 					System.out.println("switched to it");
-					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.SHORTCUT_DOWN);
 					break;
 				default:
 					Locale.setDefault(Locale.ENGLISH);
 					System.out.println("switched to en");
-					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+					keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.SHORTCUT_DOWN);
 					break;
 			}
 		}
 		else {
-			keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.CONTROL_DOWN);
+			keyCombUNDO = new KeyCodeCombination(KeyCode.Z, KeyCodeCombination.SHORTCUT_DOWN);
 		}
 
 		undo.setAccelerator(keyCombUNDO);
