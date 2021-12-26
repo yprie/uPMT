722003b2006804e64adfa4c84aab9fc1e2f1f8a4
Gabriel JOLLY <gabriel-jolly@orange.fr>
2021-12-26 19:35:37 +0100

    :bug: patched properly save '*' bug

--- src/main/java/application/appCommands/ProjectSavingStatusChangedCommand.java
+++ src/main/java/application/appCommands/ProjectSavingStatusChangedCommand.java
@@ -19,21 +19,15 @@ public class ProjectSavingStatusChangedCommand extends ApplicationCommand<Void> 
         String currentTitle = upmtApp.getPrimaryStage().getTitle();
         UUID currentCommandId = HistoryManager.getCurrentCommandId();
         UUID lastSavedCommandId = upmtApp.getLastSavedCommandId();
-        if(currentCommandId != null ){
-            if (lastSavedCommandId == null){
-                /* Correction : on ajoute '*' seulement si '*' n'est pas déjà présent dans le titre.
-                 Patch : add '*' only if '*' is not already in the title. */
-                if (currentTitle.charAt(currentTitle.length() - 2) != '*') {
-                    upmtApp.getPrimaryStage().setTitle(currentTitle + unsavedProjectSymbol);
+
+        if(currentCommandId != null ) {
+            if (currentCommandId.equals(lastSavedCommandId)) {
+                if (currentTitle.endsWith(unsavedProjectSymbol)) {
+                    upmtApp.getPrimaryStage().setTitle(currentTitle.substring(0, currentTitle.length() - unsavedProjectSymbol.length()));
                 }
-                /* gitmoji */
-            }else {
-                if (HistoryManager.getCurrentCommandId().equals(lastSavedCommandId)) {
-                    if (currentTitle.endsWith(unsavedProjectSymbol)) {
-                        upmtApp.getPrimaryStage().setTitle(currentTitle.substring(0, currentTitle.length() - unsavedProjectSymbol.length()));
-                    }
-                } else if (!currentTitle.endsWith(unsavedProjectSymbol))
-                    upmtApp.getPrimaryStage().setTitle(currentTitle + unsavedProjectSymbol);
+            }
+            else if (!currentTitle.endsWith(unsavedProjectSymbol)) {
+                upmtApp.getPrimaryStage().setTitle(currentTitle + unsavedProjectSymbol);
             }
         }
         return null;
