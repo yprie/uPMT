fc4ea8d302a84bcb79e5fdc4902e771acba30ae0
Celian Rolland <celian.rollanc@gmail.com>
2022-02-10 21:21:21 +0100

    Modifier le chargement des fichier pour l'optimiser

--- src/main/java/persistency/ProjectLoader.java
+++ src/main/java/persistency/ProjectLoader.java
@@ -7,26 +7,27 @@ import persistency.newSaveSystem.serialization.SerializationPool;
 import persistency.newSaveSystem.serialization.json.JSONReadPool;
 import persistency.newSaveSystem.serialization.json.JSONSerializer;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 public class ProjectLoader {
 
     public static Project load(String path) throws IOException {
-        FileReader fileReader = new FileReader(path);
-        String fileContents = "";
+        File fileToRead = new File(path);
+        String fileContents;
+        FileInputStream file_input = new FileInputStream(fileToRead);
+        byte[] content = new byte[(int) fileToRead.length()];
+        file_input.read(content);
+        file_input.close();
         int i ;
-        while((i =  fileReader.read())!=-1){
-            char ch = (char)i;
-            fileContents = fileContents + ch;
-        }
-        fileReader.close();
-
+        fileContents = new String(content, StandardCharsets.UTF_8);
         JSONObject obj = new JSONObject(fileContents);
         JSONReadPool pool = new JSONReadPool();
         SerializationPool<Integer, Object> modelsPool = new SerializationPool<>();
         JSONSerializer serializer = new JSONSerializer(obj, pool, modelsPool);
-
         SProject p = new SProject(serializer);
         p.initReading();
         return p.convertToModel();
