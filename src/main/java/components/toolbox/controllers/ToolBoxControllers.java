67925cb9eacb572a9cb05760c8fefe4692bb3eb9
Gabriel JOLLY <gabriel-jolly@orange.fr>
2022-05-02 11:45:29 +0200

    debug

--- src/main/java/components/toolbox/controllers/ToolBoxControllers.java
+++ src/main/java/components/toolbox/controllers/ToolBoxControllers.java
@@ -22,26 +22,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
 
-/* TODO
- * ??? : Bug, lorsque l'on drop un momentType dans la modélisation, son parent vaut null : modifier MoveMomentCommmand <= ????
- * split tollBoxControllers pour que ce soit plus propre
- * DONE 1 : TemplateSpaceController à mettre dans la tool box, il suffit de mettre le fxml dans celui de la tool box et changer le chemin de setting dans son .java
- * DONE 2 : Rendre le drag n drop possible dans la tool box, on vérifie qu'on y glisse bien un Moment
- * DONE 3 : Quand on drag un moment il apparait dans le modèle (mais sous forme de folder)
- * DONE 4 : Quand on drag un moment dans le tool box on peut le drag à nouveau dans la modélisation
- * DONE 5 : Copie profonde, création d'un type de moment dans la modélisation lors du drag + ajout couleur
- * DONE 6 : Pas de doublons + Pas de descripteme (Justification) + Pas de valeurs de propriétés
- * DONE 7 : Trouver une îcone pour la toolbox + historique
- * DONE 8 : Bug de copie profonde pour les catégories + Refactor des noms
- * DONE 9 : BUG on peut ajouter un truc de la toolbox vers la toolbox + curseur NONE si on ne peut pas drag
- * DONE 10 : Historique arbre gauche + toolbox lié et j'ai aussi fait en sorte que l'on puisse drag de l'arbre à gauche vers la modélisation
- * BUG : lorsqu'on change de langue ça recharge cette page et donc ça supprime tout (pcq on recharge la hbox) ...
- * DONE 11 :  renommage d'un type de moments via le modèle, mais c'est dégeu, faudra le refaire ! (la liaison dans momentType est moche, faut la faire dans le controller, y'a bcp de l'architecture à refaire)
- * DONE 12 : Refactor, correction d'un bug, passage en mvc PUR
- * DONE 12 à faire : continuer le refactor, notamment les changements dans le SchemaTreeRoot (la fonction en bas)
- * DONE 13 : vérifier un peu le code et faire en sorte que l'on puisse créer un mtc quand on glisse le moment vers l'arbre à gauche (HF) :)
- * DONE 14 : Rendre le changement de langue persistant (TOUT PASSER EN SIGNLETON + faire un methode update graphics
- */
 
 public class ToolBoxControllers extends HBox implements Initializable {
     private @FXML HBox bodyToolBox;
@@ -79,6 +59,7 @@ public class ToolBoxControllers extends HBox implements Initializable {
 
             return fxmlLoader.load();
         } catch (IOException exception) {
+            System.out.println("aaa");
             throw new RuntimeException(exception);
         }
     }
