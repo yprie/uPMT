82a40639f08ad65dccfd575b2789226244dcc6b0
Valentin <valentin.mignot@etu.univ-nantes.fr>
2023-05-17 01:50:39 +0200

    add the undo/redo option

---
+++ src/main/java/components/comparison/appCommands/MoveColumnCommand.java
@@ -0,0 +1,38 @@
+package components.comparison.appCommands;
+
+import application.history.ModelUserActionCommand;
+import javafx.beans.property.StringProperty;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+import java.util.List;
+
+public class MoveColumnCommand extends ModelUserActionCommand {
+    private final TableView<List<StringProperty>> tableView;
+    private final int fromIndex;
+    private final int toIndex;
+
+    public MoveColumnCommand(TableView<List<StringProperty>> tableView, int fromIndex, int toIndex) {
+        this.tableView = tableView;
+        this.fromIndex = fromIndex;
+        this.toIndex = toIndex;
+    }
+
+    @Override
+    public Object execute() {
+        // Perform the column reordering operation
+        TableColumn<List<StringProperty>, ?> column = tableView.getColumns().remove(fromIndex);
+        tableView.getColumns().add(toIndex, column);
+
+        return null;
+    }
+
+    @Override
+    public Object undo() {
+        // Undo the column reordering operation
+        TableColumn<List<StringProperty>, ?> column = tableView.getColumns().remove(toIndex);
+        tableView.getColumns().add(fromIndex, column);
+
+        return null;
+    }
+}
