e98a9584de83ba401165720774665bcaf7645457
Baptiste Douxami <baptiste.douxami@etu.univ-nantes.fr>
2024-02-07 10:02:13 +0100

    [FEAT] start the CategoryRowModel

--- src/main/java/models/CategoryRowModel.java
+++ src/main/java/models/CategoryRowModel.java
@@ -1,4 +1,23 @@
 package models;
 
+import java.util.List;
+
 public class CategoryRowModel {
+
+    private String name;
+    private SchemaCategory category;
+    private boolean selected;
+    private List<Interview> interviews;
+    private List<Moment> moments;
+    private List<ConcreteProperty> properties;
+    private List<String> propertiesValues;
+
+    private int nbOfUse;
+
+
+    public CategoryRowModel(SchemaCategory category){
+
+    }
+
+
 }
---
+++ src/main/java/models/CategoryRowModelList.java
@@ -0,0 +1,7 @@
+package models;
+
+import java.util.List;
+
+public class CategoryRowModelList {
+    private List<CategoryRowModel> CategoriesRowsModel;
+}
