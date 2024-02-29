67c7ac8d1128d7143d973f36e43c6326d3cf4a2f
Arthur GOBIN <arthur.gobin1@etu.univ-nantes.fr>
2024-02-29 09:30:44 +0100

    remplissage de la tableView de la page de sélection des catégories pour la normalisation

--- src/main/java/components/normalisation/controllers/NormalisationSelectionCategoriesControler.java
+++ src/main/java/components/normalisation/controllers/NormalisationSelectionCategoriesControler.java
@@ -2,11 +2,14 @@ package components.normalisation.controllers;
 
 import application.configuration.Configuration;
 import components.normalisation.SelectionCategoriesView;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 import models.CategoryRowModel;
+import models.SchemaCategory;
+import utils.GlobalVariables;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -15,9 +18,22 @@ public class NormalisationSelectionCategoriesControler implements Initializable 
 
     @FXML
     private TableView<CategoryRowModel> categoriesTable;
+    private ObservableList<CategoryRowModel> categories;
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+    // remplir la table avec les catégories
+        GlobalVariables globalVariables = GlobalVariables.getGlobalVariables();
+        for (SchemaCategory category : globalVariables.getProject().getSchemaTreeRoot().categoriesProperty()) {
+            categories.add(new CategoryRowModel(category));
+        }
+        // affichage des colonnes (pour vérification) de la tableView
+        for (int i = 0; i < categoriesTable.getColumns().size(); i++) {
+            System.out.println(categoriesTable.getColumns().get(i));
+        }
+
+        categoriesTable.setItems(categories);
+
 
 
     }
